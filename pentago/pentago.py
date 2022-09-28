e1ec38e18f6af7ff417f36e2cddbb7dd4c1cb53a
etai432 <etaizilberman15@gmail.com>
2022-09-28 12:56:01 +0300

    huh

---
+++ pentago/pentago.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random
+
+
+class Game:
+
+    def __init__(self):
+        self.board = np.full(1, 36)
+
+
+def main():
+    print("hi")
+
+
+if __name__ == '__main__':
+    main()
