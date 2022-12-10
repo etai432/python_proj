5e79508a9327d777b451d920e6d3d7b731c5ed25
etai432 <etaizilberman15@gmail.com>
2022-12-11 01:43:00 +0200

    ok

--- pong/draw_digits.py
+++ pong/draw_digits.py
@@ -8,32 +8,25 @@ pygame.init()
 width = 800
 height = 600
 
-# Create a screen with the specified dimensions
-screen = pygame.display.set_mode((width, height))
+# Create a screen with the specified dimensions in full screen mode
+screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
 
 # Set the title of the window
-pygame.display.set_caption("Pygame Digits")
+pygame.display.set_caption("Pygame Text")
 
 # Define colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Create a font for rendering the digits
-font = pygame.font.SysFont("Arial", 24)
-
-# This function will take a digit and dimensions as arguments and render it on the screen
-def render_digit(digit, width, height):
+# This function will take a string, dimensions, and position as arguments and render it on the screen
+def render_text(string, width, height, x, y):
   # Create a font with the specified dimensions
   font = pygame.font.SysFont("Arial", width, height)
 
-  # Create a surface with the specified digit rendered in white
-  surface = font.render(str(digit), True, WHITE)
+  # Create a surface with the specified string rendered in white
+  surface = font.render(string, True, WHITE)
 
-  # Calculate the x and y position of the surface on the screen
-  x = (screen.get_width() - width) / 2
-  y = (screen.get_height() - height) / 2
-
-  # Blit (draw) the surface onto the screen
+  # Blit (draw) the surface onto the screen at the specified position
   screen.blit(surface, (x, y))
 
 # Main game loop
@@ -47,11 +40,10 @@ while running:
   # Clear the screen
   screen.fill(BLACK)
 
-  # Render the digit 5 on the screen with dimensions 50x50
-  render_digit(5, 600, 600)
+  # Render the string "Hello, World!" on the screen with dimensions 50x50 at position (100, 100)
+  render_text("Hello, World!", 50, 50, 100, 100)
 
   # Update the display
   pygame.display.flip()
 
-# Close the window and clean up Pygame
-pygame.quit()
+# Close the win
