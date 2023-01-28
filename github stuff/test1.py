f4c832fb4bf658ba5fb79d17fc8d26b325b5f4a4
etai432 <etaizilberman15@gmail.com>
2023-01-28 23:11:09 +0200

    ok

--- github stuff/test1.py
+++
@@ -1,7 +0,0 @@
-from stockfish import Stockfish
-stockfish = Stockfish(path="C:/Users/User/Downloads/stockfish_15.1_win_x64_avx2/stockfish-windows-2022-x86-64-avx2.exe")
-# stockfish = Stockfish(path="/home/itay/Downloads/stockfish_15.1_linux_x64_bmi2/stockfish_15.1_x64_bmi2")
-print(stockfish.set_fen_position("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2"))
-print(stockfish.get_best_move())
-print(stockfish.get_board_visual())
-print(stockfish.get_evaluation())
