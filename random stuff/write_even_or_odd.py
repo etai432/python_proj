757169eedfd15fd165d203e1e7ac264f1a792e9e
etai432 <etaizilberman15@gmail.com>
2023-03-24 00:50:18 +0200

    deleteing a bunch of stuff

--- random stuff/write_even_or_odd.py
+++
@@ -1,7 +0,0 @@
-for i in range(1, 2001):
-    if i % 2 == 0:
-        print("elif i == " + str(i) + ":")
-        print('\tprint("even")')
-    else:
-        print("elif i == " + str(i) + ":")
-        print('\tprint("odd")')
