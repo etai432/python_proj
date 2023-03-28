8cfacc305f39642dedd5a6efaaf85e80733c5cae
etai432 <etaizilberman@gmail.com>
2023-03-28 13:35:39 +0300

    ok

--- pong/pong.py
+++ pong/pong.py
@@ -8,6 +8,7 @@ import random
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
+
 class Paddle:
     def __init__(self, length, width, posx, posy, direction, screen):
         self.start_y = (posx, posy)
@@ -19,11 +20,11 @@ class Paddle:
         self.screen = screen
         self.score = 0
         self.rect = pygame.Rect(self.posx, self.posy, self.width, self.length)
-    
+
     def reset(self):
         self.posx = self.start_y[0]
         self.posy = self.start_y[1]
-    
+
     def action(self, action):
         if action == 0:
             self.posy -= 5
@@ -33,7 +34,7 @@ class Paddle:
             self.posy += 5
             if self.posy > self.screen[1] - self.length:
                 self.posy = self.screen[1] - self.length
-    
+
     def update_rect(self):
         self.rect = pygame.Rect(self.posx, self.posy, self.width, self.length)
 
@@ -52,10 +53,10 @@ class Ball:
         self.extra_x = extra_x
         self.extra_y = extra_y
         self.radius = radius
-    
+
     def copy(self):
         return Ball(self.posx, self.posy, self.speed, self.max_speed, self.dx, self.dy, self.direction_x, self.radius, self.screen, self.extra_x, self.extra_y)
-    
+
     def reset(self):
         self.posx = self.starting_state[0]
         self.posy = self.starting_state[1]
@@ -105,26 +106,28 @@ class Ball:
         angle = distance / (length-2) * math.pi * 1 / 2
         self.dx = math.cos(angle) * self.direction_x * self.speed
         self.dy = math.sin(angle) * self.speed
-    
+
     def change_speed(self):
         if self.speed < self.max_speed:
             self.speed += 0.2
+
 
 class Env():
     def __init__(self):
         self.screen = (800, 600)
         self.max_steps = 25000
-        self.model = self.make_model('pong/pong_model.h5')
+        self.model = self.make_model('python_proj/pong/pong_model.h5')
         # self.model = self.make_model()
         self.memory = []
-        # with open(f"pong/memory1.pickle", "rb") as f:
-            # self.memory = pickle.load(f)
-        with open(f"pong/scaler.pickle", "rb") as f:
+        # with open(f"pythong_proj/pong/memory1.pickle", "rb") as f:
+        # self.memory = pickle.load(f)
+        with open(f"python_proj/pong/scaler.pickle", "rb") as f:
             self.scaler = pickle.load(f)
         self.paddle1 = Paddle(100, 5, 50, 265, 1, self.screen)
         self.paddle2 = Paddle(100, 5, 750, 265, -1, self.screen)
         num1 = np.random.randint(0, 2)
-        self.ball = Ball(400, 300, 5, 10, num1 * 2 - 1, 0, num1 * 2 - 1, 3, self.screen)
+        self.ball = Ball(400, 300, 5, 10, num1 * 2 - 1,
+                         0, num1 * 2 - 1, 3, self.screen)
 
     def check_win(self):
         if self.ball.posx == 0:
@@ -132,12 +135,12 @@ class Env():
         if self.ball.posx == self.screen[0] - self.ball.radius:
             return 2
         return 1
-    
+
     def new_point(self):
         self.ball.reset()
         self.paddle1.reset()
         self.paddle2.reset()
-    
+
     def new_game(self):
         self.new_point()
         self.paddle1.score = 0
@@ -152,21 +155,25 @@ class Env():
         while self.paddle1.score < 10 and self.paddle2.score < 10 and counter < self.max_steps:
             counter += 1
             self.ball.update()
-            act = np.argmax(self.model.predict_on_batch(np.array([[self.ball.posx, self.ball.posy, self.paddle1.posy + self.paddle1.length/2, self.ball.dx, self.ball.dy]]))[0])
+            act = np.argmax(self.model.predict_on_batch(np.array(
+                [[self.ball.posx, self.ball.posy, self.paddle1.posy + self.paddle1.length/2, self.ball.dx, self.ball.dy]]))[0])
             self.paddle1.action(act)
             self.teleport2()
-            memory_x.append([self.ball.posx, self.ball.posy, self.paddle1.posy + self.paddle1.length/2, self.ball.dx, self.ball.dy])
+            memory_x.append([self.ball.posx, self.ball.posy, self.paddle1.posy +
+                            self.paddle1.length/2, self.ball.dx, self.ball.dy])
             memory_y.append(self.get_target())
             if self.ball.posx >= self.paddle1.posx and self.ball.posx <= self.paddle1.posx + self.ball.speed:
                 if self.ball.posy >= self.paddle1.posy and self.ball.posy <= (self.paddle1.posy + self.paddle1.length):
                     self.ball.change_speed()
-                    self.ball.hit_paddle(self.ball.posy - self.paddle1.posy + self.ball.radius//2, self.paddle1.length)
+                    self.ball.hit_paddle(
+                        self.ball.posy - self.paddle1.posy + self.ball.radius//2, self.paddle1.length)
                     self.ball.posx = self.paddle1.posx + self.paddle1.width
                     touch += 1
             if self.ball.posx + self.ball.radius - 1 >= self.paddle2.posx and self.ball.posx + self.ball.radius - 1 <= self.paddle2.posx + self.ball.speed:
                 if self.ball.posy >= self.paddle2.posy and self.ball.posy <= (self.paddle2.posy + self.paddle2.length):
                     self.ball.change_speed()
-                    self.ball.hit_paddle(self.ball.posy - self.paddle2.posy + self.ball.radius//2, self.paddle2.length)
+                    self.ball.hit_paddle(
+                        self.ball.posy - self.paddle2.posy + self.ball.radius//2, self.paddle2.length)
                     self.ball.posx = self.paddle2.posx - self.ball.radius
             if self.check_win() == 0:
                 self.paddle2.score += 1
@@ -195,7 +202,8 @@ class Env():
         while self.paddle1.score < 10 and self.paddle2.score < 10 and counter < self.max_steps:
             counter += 1
             self.ball.update()
-            state = [self.ball.posx, self.ball.posy, self.paddle1.posy + self.paddle1.length/2, self.ball.dx, self.ball.dy, self.ball.extra_x, self.ball.extra_y]
+            state = [self.ball.posx, self.ball.posy, self.paddle1.posy + self.paddle1.length /
+                     2, self.ball.dx, self.ball.dy, self.ball.extra_x, self.ball.extra_y]
             if counter % 3 == 0:
                 if self.get_target() != [0, 1, 0] or np.random.rand() > 0.8:
                     memory_x.append(state)
@@ -208,13 +216,15 @@ class Env():
             if self.ball.posx >= self.paddle1.posx and self.ball.posx <= self.paddle1.posx + self.ball.speed:
                 if self.ball.posy >= self.paddle1.posy and self.ball.posy <= (self.paddle1.posy + self.paddle1.length):
                     self.ball.change_speed()
-                    self.ball.hit_paddle(self.ball.posy - self.paddle1.posy + self.ball.radius//2, self.paddle1.length)
+                    self.ball.hit_paddle(
+                        self.ball.posy - self.paddle1.posy + self.ball.radius//2, self.paddle1.length)
                     self.ball.posx = self.paddle1.posx + self.paddle1.width
                     touch += 1
             if self.ball.posx + self.ball.radius - 1 >= self.paddle2.posx and self.ball.posx + self.ball.radius - 1 <= self.paddle2.posx + self.ball.speed:
                 if self.ball.posy >= self.paddle2.posy and self.ball.posy <= (self.paddle2.posy + self.paddle2.length):
                     self.ball.change_speed()
-                    self.ball.hit_paddle(self.ball.posy - self.paddle2.posy + self.ball.radius//2, self.paddle2.length)
+                    self.ball.hit_paddle(
+                        self.ball.posy - self.paddle2.posy + self.ball.radius//2, self.paddle2.length)
                     self.ball.posx = self.paddle2.posx - self.ball.radius
             if self.check_win() == 0:
                 self.paddle2.score += 1
@@ -229,19 +239,26 @@ class Env():
         if self.paddle1.score == 10:
             return 0
         return 2
-    
+
     def teleport2(self):
         if np.random.rand() > 0.5:
-                self.paddle2.posy = self.ball.posy - self.paddle2.length/2 + np.random.randint(-self.paddle2.length/2, self.paddle2.length/2)
+            self.paddle2.posy = self.ball.posy - self.paddle2.length/2 + \
+                np.random.randint(-self.paddle2.length/2,
+                                  self.paddle2.length/2)
         else:
             if np.random.rand() > 0.5:
-                self.paddle2.posy = self.ball.posy - self.paddle2.length/2 + np.random.randint(self.paddle2.length/2 - 10, self.paddle2.length/2)
+                self.paddle2.posy = self.ball.posy - self.paddle2.length/2 + \
+                    np.random.randint(self.paddle2.length /
+                                      2 - 10, self.paddle2.length/2)
             else:
-                self.paddle2.posy = self.ball.posy - self.paddle2.length/2 + np.random.randint(-self.paddle2.length/2, -self.paddle2.length/2 + 10)
-    
+                self.paddle2.posy = self.ball.posy - self.paddle2.length/2 + \
+                    np.random.randint(-self.paddle2.length /
+                                      2, -self.paddle2.length/2 + 10)
+
     def update_env(self, game_type):
         self.ball.update()
-        state = [self.ball.posx, self.ball.posy, self.paddle1.posy + self.paddle1.length/2, self.ball.dx, self.ball.dy, self.ball.extra_x, self.ball.extra_y]
+        state = [self.ball.posx, self.ball.posy, self.paddle1.posy + self.paddle1.length /
+                 2, self.ball.dx, self.ball.dy, self.ball.extra_x, self.ball.extra_y]
         norm_state = self.scaler.transform([state])
         if game_type == 1:
             act = np.argmax(self.model.predict_on_batch(norm_state)[0])
@@ -249,19 +266,22 @@ class Env():
         elif game_type == 3:
             act = np.argmax(self.model.predict_on_batch(norm_state)[0])
             self.paddle1.action(act)
-            state = [self.screen[0] - self.ball.posx, self.ball.posy, self.paddle2.posy + self.paddle2.length/2, -self.ball.dx, self.ball.dy, -self.ball.extra_x, self.ball.extra_y]
+            state = [self.screen[0] - self.ball.posx, self.ball.posy, self.paddle2.posy +
+                     self.paddle2.length/2, -self.ball.dx, self.ball.dy, -self.ball.extra_x, self.ball.extra_y]
             norm_state = self.scaler.transform([state])
             act = np.argmax(self.model.predict_on_batch(norm_state)[0])
             self.paddle2.action(act)
         if self.ball.posx >= self.paddle1.posx and self.ball.posx <= self.paddle1.posx + self.ball.speed:
             if self.ball.posy >= self.paddle1.posy and self.ball.posy <= (self.paddle1.posy + self.paddle1.length):
                 self.ball.change_speed()
-                self.ball.hit_paddle(self.ball.posy - self.paddle1.posy + self.ball.radius//2, self.paddle1.length)
+                self.ball.hit_paddle(
+                    self.ball.posy - self.paddle1.posy + self.ball.radius//2, self.paddle1.length)
                 self.ball.posx = self.paddle1.posx + self.paddle1.width
         if self.ball.posx + self.ball.radius - 1 >= self.paddle2.posx and self.ball.posx + self.ball.radius - 1 <= self.paddle2.posx + self.ball.speed:
             if self.ball.posy >= self.paddle2.posy and self.ball.posy <= (self.paddle2.posy + self.paddle2.length):
                 self.ball.change_speed()
-                self.ball.hit_paddle(self.ball.posy - self.paddle2.posy + self.ball.radius//2, self.paddle2.length)
+                self.ball.hit_paddle(
+                    self.ball.posy - self.paddle2.posy + self.ball.radius//2, self.paddle2.length)
                 self.ball.posx = self.paddle2.posx - self.ball.radius
         if self.check_win() == 0:
             self.paddle2.score += 1
@@ -291,7 +311,7 @@ class Env():
         elif d < -5:
             return [0, 0, 1]
         return [0, 1, 0]
-    
+
     def get_moves(self):
         end1 = self.predict_hit_y()
         d = self.paddle1.posy + self.paddle1.length/2 - end1
@@ -304,7 +324,7 @@ class Env():
         elif d < -5:
             return [0, 0, 1]
         return [0, 1, 0]
-    
+
     def ai2(self):
         if self.ball.posy > self.paddle2.posy + self.paddle2.length - 8:
             return 2
@@ -312,7 +332,7 @@ class Env():
             return 0
         else:
             return np.random.randint(0, 3)
-        
+
     def make_model(self, path=None):
         if path == None:
             model = tf.keras.Sequential()
@@ -325,33 +345,37 @@ class Env():
             model.add(tf.keras.layers.Dense(64, activation='relu'))
             model.add(tf.keras.layers.Dense(32, activation='relu'))
             model.add(tf.keras.layers.Dense(3, activation="softmax"))
-            model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss='categorical_crossentropy')
+            model.compile(optimizer=tf.keras.optimizers.Adam(
+                learning_rate=0.0001), loss='categorical_crossentropy')
             return model
         else:
             model = tf.keras.models.load_model(path)
             return model
-    
+
     def train_model(self):
         scaler = MinMaxScaler()
         x = np.concatenate([i[0] for i in self.memory])
         y = np.concatenate([i[1] for i in self.memory])
         x = scaler.fit_transform(x)
-        x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.1)
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y, test_size=0.1)
         x_train = np.array_split(x_train, 100)
         y_train = np.array_split(y_train, 100)
         for i in range(100):
-            print(i,"% complete")
-            self.model.fit(x_train[i], y_train[i], epochs=2, validation_split=0.05)
-            self.model.save("pong/pong_model1.h5")
+            print(i, "% complete")
+            self.model.fit(x_train[i], y_train[i],
+                           epochs=2, validation_split=0.05)
+            self.model.save("python_proj/pong/pong_model.h5")
         print(self.model.evaluate(x_test, y_test))
-        with open(f"pong/scaler1.pickle", "wb") as f:
+        with open(f"python_proj/pong/scaler.pickle", "wb") as f:
             pickle.dump(scaler, f)
 
     def save_model(self):
-        self.model.save("pong/pong_model1.h5")
+        self.model.save("python_proj/pong/pong_model.h5")
         with open(f"pong/memory2.pickle", "wb") as f:
             pickle.dump(self.memory, f)
-        
+
+
 def main():
     env = Env()
     for i in range(300):
@@ -368,4 +392,5 @@ if __name__ == "__main__":
     start = time.time()
     main()
     end = time.time() - start
-    print(f"the program took {int(end // 60)} minutes and {int(end % 60)} seconds to finish")
+    print(
+        f"the program took {int(end // 60)} minutes and {int(end % 60)} seconds to finish")
